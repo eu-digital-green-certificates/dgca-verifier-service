b8db2ffc6cea01355b9fc04187a159fc6bc1a2cc
Simon Laurenz <simon.laurenz@t-systems.com>
2021-04-20 18:09:34 +0200

    Copy base structure from  Digital Green Certificate Gateway

---
+++ src/main/java/eu/europa/ec/dgc/verifier/restapi/controller/RuleInformationController.java
@@ -0,0 +1,10 @@
+package eu.europa.ec.dgc.verifier.restapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RuleInformationController {
+
+}
