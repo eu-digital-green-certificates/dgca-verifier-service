a8369f2c831af2bd5327bb4b7cfe589a73c50180
Simon Laurenz <simon.laurenz@t-systems.com>
2021-04-29 09:38:14 +0200

    changed import order for checkstyle

--- src/main/java/eu/europa/ec/dgc/verifier/entity/SignerInformationEntity.java
+++ src/main/java/eu/europa/ec/dgc/verifier/entity/SignerInformationEntity.java
@@ -20,19 +20,18 @@
 
 package eu.europa.ec.dgc.verifier.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import java.time.ZonedDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @Getter
