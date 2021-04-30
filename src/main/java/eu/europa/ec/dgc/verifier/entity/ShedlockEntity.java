e1bd2b8db4116f34f1bac928f26f76ea24674a86
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-04-30 15:33:24 +0200

    Add Liquibase Changelog Add Shedlock Entity

---
+++ src/main/java/eu/europa/ec/dgc/verifier/entity/ShedlockEntity.java
@@ -0,0 +1,46 @@
+/*-
+ * ---license-start
+ * eu-digital-green-certificates / dgca-verifier-service
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
+package eu.europa.ec.dgc.verifier.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shedlock")
+public class ShedlockEntity {
+
+    @Id
+    @Column(name = "name", length = 64, nullable = false)
+    private String name;
+
+    @Column(name = "lock_until", nullable = false)
+    private Date lockUntil;
+
+    @Column(name = "locked_at", nullable = false)
+    private Date lockedUntil;
+
+    @Column(name = "locked_by", length = 255, nullable = false)
+    private Date lockedBy;
+
+}
