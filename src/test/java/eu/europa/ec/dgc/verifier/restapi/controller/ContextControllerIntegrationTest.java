4cfed2390e7b3d3d52ed150eada5272d83e27c33
Simon Laurenz <simon.laurenz@t-systems.com>
2021-05-19 17:42:25 +0200

    Code smell

--- src/test/java/eu/europa/ec/dgc/verifier/restapi/controller/ContextControllerIntegrationTest.java
+++ src/test/java/eu/europa/ec/dgc/verifier/restapi/controller/ContextControllerIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ContextControllerIntegrationTest {
+class ContextControllerIntegrationTest {
 
     @MockBean
     DgcGatewayDownloadConnector dgcGatewayDownloadConnector;
